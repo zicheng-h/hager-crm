259e9908382d4ea24081d15d6c8448ce6c3a0267
Zicheng Huang <zhuang@niagaracollege.ca>
2021-01-28 23:33:24 -0500

    Adjusted menu items.
    -Created Configuration view and controller.

---
+++ hager-crm/Controllers/ConfigurationController.cs
@@ -0,0 +1,30 @@
+﻿using hager_crm.Data;
+using hager_crm.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hager_crm.Controllers
+{
+    public class ConfigurationController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public ConfigurationController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
