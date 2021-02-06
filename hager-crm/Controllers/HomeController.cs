eae8919960756bc9bcf56ed035f875acba771dfe
Danylo Kukanov <dkukanov1@ncstudents.niagaracollege.ca>
2021-02-06 14:01:37 -0500

    Disabled registration. Fixed login behavior. Changed css for login

--- hager-crm/Controllers/HomeController.cs
+++ hager-crm/Controllers/HomeController.cs
@@ -6,9 +6,11 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 
 namespace hager_crm.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
