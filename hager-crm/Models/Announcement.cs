f507179609c5b73f65eb814ac9f7098ae991ad0b
Danylo Kukanov <dkukanov1@ncstudents.niagaracollege.ca>
2021-04-07 03:24:33 -0400

    Clear imports

--- hager-crm/Models/Announcement.cs
+++ hager-crm/Models/Announcement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
-using OfficeOpenXml.FormulaParsing.Excel.Functions.DateTime;
+
 
 namespace hager_crm.Models
 {
--- hager-crm/Models/CompanyContractor.cs
+++ hager-crm/Models/CompanyContractor.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
-using OfficeOpenXml.FormulaParsing.Excel.Functions.DateTime;
 
 namespace hager_crm.Models
 {
